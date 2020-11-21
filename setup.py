a37cff0f515411db40462063835a64d18f1f1b2e
Evgeny Naumov <evgeny.naumov@protonmail.com>
2020-11-21 00:59:24 -0500

    Initial commit

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='py9lib',
+    version='0.5',
+    packages=['py9lib'],
+    url='http://github.com/qdbp/py9lib.git',
+    license='',
+    author='Evgeny Naumov',
+    author_email='egeny@enaumov.me',
+    description='utility code'
+)
